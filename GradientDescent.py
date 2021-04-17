7f34a9d3eb7ebd8730fecba436aa1a9d1abb1e5d
Li-O-H <60929791+Li-O-H@users.noreply.github.com>
2021-04-18 00:14:26 +0300

    Add files via upload
    Добавил рисование графиков (рисует график функции с траекторией метода и линии уровня с траекторией метода). Сделал еще градиентный спуск (но насчет его правильности не уверен)

---
+++ GradientDescent.py
@@ -0,0 +1,92 @@
+import math
+import matplotlib.pyplot as plt
+import pylab
+import numpy as np
+from enum import Enum
+
+n = 2
+fragmentationCoef = 0.2
+fragmentationLimitCoef = 0.1
+
+
+class StepFindingMethod(Enum):
+    constantStep = 0
+    stepFragmentation = 1
+
+
+def f(x):
+    return x[0] ** 2 + x[1] ** 2 + x[0] * x[1]
+
+
+def grad_f(x):
+    return [2 * x[0] + x[1] ** 2 + x[1], x[0] ** 2 + 2 * x[1] + x[0]]
+
+
+def norma(x):
+    summa = 0
+    for i in range(n):
+        summa += x[i] ** 2
+    return math.sqrt(summa)
+
+
+x = []
+alpha_arr = []
+
+
+def substractPoints(x1, x2):
+    result = []
+    for i in range(len(x1)):
+        result.append(x1[i] - x2[i])
+    return result
+
+
+# Метод градиентного спуска
+def GradientDescent(x_start, alpha_start, epsilon, step_finding_method):
+    x.append(x_start)
+    alpha = alpha_start
+    while True:
+        if step_finding_method == StepFindingMethod.stepFragmentation:
+            if len(x) % 100 == 0:
+                alpha = alpha_start
+            while True:
+                x_new = []
+                for i in range(n):
+                    x_new.append(x[len(x) - 1][i] - alpha * grad_f(x[len(x) - 1])[i])
+                if f(x_new) <= f(x[len(x) - 1]) - fragmentationLimitCoef * alpha * (norma(grad_f(x[len(x) - 1])) ** 2):
+                    break
+                alpha *= fragmentationCoef
+        alpha_arr.append(alpha)
+        x_new = []
+        for i in range(n):
+            x_new.append(x[len(x) - 1][i] - alpha * grad_f(x[len(x) - 1])[i])
+        x.append(x_new)
+        if norma(substractPoints(x[len(x) - 1], x[len(x) - 2])) < epsilon:
+            break
+
+
+GradientDescent([1, -2], 1, 0.0005, StepFindingMethod.stepFragmentation)
+
+f_arr = []
+for i in range(len(x)):
+    f_arr.append(f(x[i]))
+
+x1 = []
+x2 = []
+for i in range(len(x)):
+    x1.append(x[i][0])
+    x2.append(x[i][1])
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+X1 = np.arange(min(x1), max(x1), 0.1)
+X2 = np.arange(min(x2), max(x2), 0.1)
+X1, X2 = np.meshgrid(X1, X2)
+Z = f([X1, X2])
+ax.plot_surface(X1, X2, f([X1, X2]), alpha=0.5)
+ax.plot(x1, x2, f_arr, color="black")
+plt.show()
+f_arr.sort()
+levels = pylab.contour(X1, X2, Z, f_arr)
+pylab.clabel(levels)
+pylab.plot(x1, x2)
+pylab.show()
--- SteepestDescent.py
+++ SteepestDescent.py
@@ -1,16 +1,25 @@
 import math
+import matplotlib.pyplot as plt
+import pylab
+import numpy as np
+from enum import Enum
+
+
+class StepFindingMethod(Enum):
+    goldenSection = 0
+    fibonacci = 1
 
 
 def f(x1, x2):
-    return x1 ** 2 - x2 ** 2
+    return x1 ** 2 + x2 ** 2 + x1 * x2
 
 
 def df_dx1(x1, x2):
-    return 2 * x1 - x2 ** 2
+    return 2 * x1 + x2 ** 2 + x2
 
 
 def df_dx2(x1, x2):
-    return x1 ** 2 - 2 * x2
+    return x1 ** 2 + 2 * x2 + x1
 
 
 def g(x1, x2, alpha):
@@ -29,7 +38,7 @@ def Fibonacci(n):
 # Метод Фибоначчи
 def Fibonacci_Method(x_min, x_max, number_of_iterations, x1, x2, iteration=0):
     if iteration == number_of_iterations:
-        return (x_max - x_min) / 2 + x_min
+        return (x_max + x_min) / 2
     x_lhs = x_min + (((x_max - x_min) * Fibonacci(number_of_iterations - iteration - 1)) / Fibonacci(
         number_of_iterations - iteration + 1))
     x_rhs = x_min + (((x_max - x_min) * Fibonacci(number_of_iterations - iteration)) / Fibonacci(
@@ -62,22 +71,48 @@ def GoldenSectionMethod(x_min, x_max, epsilon, x1, x2):
             sc = sd
             sd = g(x1, x2, d)
         iteration += 1
-    return (x_max - x_min) / 2 + x_min
+    return (x_max + x_min) / 2
+
+
+x1 = []
+x2 = []
+alpha_arr = []
 
 
 # Метод наискорейшего спуска
-def SteepestDescent(x1_start, x2_start, epsilon):
-    # n = x.length
-    x1 = []
-    x2 = []
-    alpha_arr = []
+def SteepestDescent(x1_start, x2_start, epsilon, step_finding_method):
     x1.append(x1_start)
     x2.append(x2_start)
     while True:
-        alpha_arr.append(Fibonacci_Method(0, 100, 26, x1[len(x1) - 1], x2[len(x2) - 1]))
-        # alpha_arr.append(GoldenSectionMethod(0, 100, epsilon, x1[len(x1) - 1], x2[len(x2) - 1]))
-        x1.append(x1[len(x1) - 1] - alpha_arr[len(alpha_arr) - 1] * df_dx1(x1[len(x1) - 1], x2[len(x2) - 1]))
-        x2.append(x2[len(x2) - 1] - alpha_arr[len(alpha_arr) - 1] * df_dx2(x1[len(x1) - 1], x2[len(x2) - 1]))
+        if step_finding_method == StepFindingMethod.goldenSection:
+            alpha_arr.append(GoldenSectionMethod(0, 1000000000, epsilon, x1[len(x1) - 1], x2[len(x2) - 1]))
+        if step_finding_method == StepFindingMethod.fibonacci:
+            alpha_arr.append(Fibonacci_Method(0, 1000000000, 100, x1[len(x1) - 1], x2[len(x2) - 1]))
+        x1_new = x1[len(x1) - 1] - alpha_arr[len(alpha_arr) - 1] * df_dx1(x1[len(x1) - 1], x2[len(x2) - 1])
+        x2_new = x2[len(x2) - 1] - alpha_arr[len(alpha_arr) - 1] * df_dx2(x1[len(x1) - 1], x2[len(x2) - 1])
+        x1.append(x1_new)
+        x2.append(x2_new)
         if norma(x1[len(x1) - 1] - x1[len(x1) - 2], x2[len(x2) - 1] - x2[len(x2) - 2]) < epsilon:
             break
-    print(x1[len(x1) - 1], " ", x2[len(x2) - 1])
+
+
+SteepestDescent(-4, 1, 0.0001, StepFindingMethod.goldenSection)
+
+f_arr = []
+for i in range(len(x1)):
+    f_arr.append(f(x1[i], x2[i]))
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+X1 = np.arange(min(x1), max(x1), 0.1)
+X2 = np.arange(min(x2), max(x2), 0.1)
+X1, X2 = np.meshgrid(X1, X2)
+Z = f(X1, X2)
+ax.plot_surface(X1, X2, f(X1, X2), alpha=0.5)
+ax.plot(x1, x2, f_arr, color="black")
+plt.show()
+f_arr.sort()
+levels = pylab.contour(X1, X2, Z, f_arr)
+pylab.clabel(levels)
+pylab.plot(x1, x2)
+pylab.show()
